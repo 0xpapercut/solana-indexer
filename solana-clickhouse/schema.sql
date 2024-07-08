26ae35cd761fff156e63665331d1bb38161f9d0e
0xpapercut <0xpapercut@gmail.com>
2024-07-08 13:14:21 +0200

    Update schema.sql

--- solana-clickhouse/schema.sql
+++ solana-clickhouse/schema.sql
@@ -1,39 +1,14 @@
-CREATE TABLE IF NOT EXISTS "raydium_swap_events"
+CREATE TABLE raydium_swap_events
 (
-    signature VARCHAR(64),
+    signature VARCHAR(88),
+    id UInt64,
     slot UInt64,
-    event_type VARCHAR(10),
-    -- events Nested (
-    --     amm VARCHAR(64),
-    --     user VARCHAR(64),
-        -- data Nested (
-        --     initialize Nested (
-        --         pc_init_amount UInt64,
-        --         coin_init_amount UInt64,
-        --         lp_init_amount UInt64,
-        --         pc_mint VARCHAR(64),
-        --         coin_mint VARCHAR(64),
-        --         lp_mint VARCHAR(64),
-        --         nonce UInt32
-        --     ),
-        --     deposit Nested (
-        --         pc_amount UInt64,
-        --         coin_amount UInt64,
-        --         lp_amount UInt64
-        --     ),
-        --     withdraw Nested (
-        --         pc_amount UInt64,
-        --         coin_amount UInt64,
-        --         lp_amount UInt64
-        --     ),
-        --     swap Nested (
-        --         mint_in VARCHAR(64),
-        --         mint_out VARCHAR(64),
-        --         amount_in UInt64,
-        --         amount_out UInt64
-        --     )
-        -- )
-    -- )
+    amm VARCHAR(44),
+    user VARCHAR(44),
+    amount_in UInt64,
+    amount_out UInt64,
+    mint_in VARCHAR(44),
+    mint_out VARCHAR(44),
 )
 ENGINE = MergeTree
-PRIMARY KEY (hash)
+PRIMARY KEY (signature, id)
