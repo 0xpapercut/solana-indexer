3c99d109be8b2a19f63fccc3a8bc95e7f26c719e
0xpapercut <0xpapercut@gmail.com>
2024-10-17 10:54:37 +0200

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -10,8 +10,8 @@ CREATE FUNCTION getPumpfunMigrationRaydiumAMM AS (mint) -> (
     ) AND signer0 = pumpfunRaydiumMigrationSigner()
 );
 
-DROP FUNCTION IF EXISTS raydiumProgramID;
-CREATE FUNCTION raydiumProgramID AS () -> '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8';
+DROP FUNCTION IF EXISTS raydiumAmmProgramID;
+CREATE FUNCTION raydiumAmmProgramID AS () -> '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8';
 
 DROP FUNCTION IF EXISTS raydiumAuthority;
 CREATE FUNCTION raydiumAuthority AS () -> '5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1';
